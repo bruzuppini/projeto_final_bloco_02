7ef8cd4138a46641eef0fa9eab72216e164e6b49
Bruna Zuppini <brunazuppini@gmail.com>
2026-05-15 11:43:43 -0300

    fix: conexão banco render

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,18 @@ async function bootstrap() {
   process.env.TZ = '-03:00';
 
   app.useGlobalPipes(new ValidationPipe());
-
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Farmácia API')
+    .setDescription('Documentação da API da Farmácia')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap(
-  const config = new DocumentBuilder()
-  .setTitle('Farmácia API')
-  .setDescription('Documentação da API da Farmácia')
-  .setVersion('1.0')
-  .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('swagger', app, document);
-);
+bootstrap();
